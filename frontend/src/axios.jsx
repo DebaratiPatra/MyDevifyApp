d35e13796e747e3759055412f8551aad9840def1
DebaratiPatra <debaratipatra2004@gmail.com>
2025-10-08 11:47:45 +0530

    Update axios.jsx

--- frontend/src/axios.jsx
+++ frontend/src/axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const makeRequest = axios.create({
-    baseURL: "https://mydevifyapp-backend.onrender.com",
+    baseURL: "https://mydevifyapp-backend.onrender.com/api",
     withCredentials: true,
 })
